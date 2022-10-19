2365075087e46ffcf688f8ef711a04e8ff8fb064
antoine MAYSLICH <antoinemayslich@Atlantis.local>
2022-10-19 23:13:48 +0200

    Update pre flight script to install python and clone the repo

--- setup.sh
+++ setup.sh
@@ -2,27 +2,61 @@
 # This function is used when you need to know the error location in the running code
 set -e
 
-echo ".dotfile setup for macOS \n"
-echo "\n 1 - Verify that Ansible dependencies are available \n"
+if ! ping -c 3 www.google.fr
+then
+    echo "An internet connexion is required to launch this script."
+    exit 1
+fi
+
+echo
+echo "1️⃣ Install Ansible and his dependencies"
+echo
 
 # Install Xcode Command-Line Tools
 if ! xcode-select -p ; then
 	xcode-select --install
 	exit 0
 fi
-echo "		1/3 Xcode CLI tools is ready ✅"
+echo
+echo "		1/6 Xcode CLI tools is ready ✅"
+echo
 
 # Install & update Homebrew
 brew --version || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew doctor || exit $?
 brew update
-echo "		2/3 Homebrew is ready ✅"
+echo
+echo "		2/6 Homebrew is ready ✅"
+echo
+
+# Install git
+# echo
+# git --version || brew install git
+# echo
+# echo "		3/5 Git is ready ✅"
+
+# Install python
+python3 --version || brew install python
+echo
+echo "		4/6 Python is ready ✅"
+echo
 
 # Install Ansible
 ansible --version || brew install ansible
-echo "		3/3 Ansible is ready ✅"
+echo
+echo "		5/6 Ansible is ready ✅"
+echo
 
-echo "\n 2 - Launch Ansible script \n"
-ansible-playbook macbook_setup.yml --ask-become-pass --ask-vault-pass
+# Clone the repo
+git clone git@github.com:Synodra/ansible-osx-setup.git
+echo
+echo "      6/6 Git repo has been cloned ✅"
 
-echo "\n DONE - macOS is ready ✅ \n"
+# echo
+# echo "2️⃣ Launch Ansible script"
+# echo
+# ansible-playbook ansible-osx-setup/playbook.yml --ask-become-pass --ask-vault-pass
+# 
+# echo
+# echo "DONE - macOS is ready 🎉"
+# echo
